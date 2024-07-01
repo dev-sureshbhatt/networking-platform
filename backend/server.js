6ab6d4e67b8fbc4aae388024700d712ed3e61ffd
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-07-01 09:47:46 +0530

    Prepared app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import {v2 as cloudinary} from 'cloudinary'
+import path from 'path'
 
 import authRouter from './routes/auth.route.js'
 import userRouter from './routes/user.route.js'
@@ -23,6 +24,7 @@ cloudinary.config({
 
 const app = express()
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
 
 app.use(express.json({limit: '4mb'})) // for image files, we need to still verify if this is secure or not
 app.use(express.urlencoded({extended: true}))
@@ -36,6 +38,17 @@ app.use('/api/auth', authRouter)
 app.use('/api/users', userRouter)
 app.use('/api/posts', postRouter)
 app.use('/api/notifications', notificationRouter)
+
+
+
+
+if (process.env.NODE_ENV === "production"){
+    
+    app.use(express.static(path.join(__dirname, '/client/dist')));
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+    })
+}
 
 
 
